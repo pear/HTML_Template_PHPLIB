23aa44c60894395370d745536497b07115f95159
Bjoern Schotte <bjoern@php.net>
2003-05-29 12:54:11 +0000

    beginning some examples
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_PHPLIB/trunk@129147 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/tpl1.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once 'HTML/Template/PHPLIB.php';
+
+/**
+* "keep" = keep unknown template variables,
+* "remove" = remove 'em,
+* "comment" = put them into comments
+*
+* use keep if you have stylesheets or JavaScript
+* in your HTML code since those are also using
+* { and } which will then get removed.
+*/
+$t =& new Template_PHPLIB("./", "keep");
+
+$t->setFile(array(
+	"main" => "tpl1.ihtml"
+));
+
+$welcome = "Welcome to the real world";
+
+$t->setVar(array(
+	"TITLE" => "This is my title",
+	"BGCOLOR" => "#cccccc",
+	"CONTENT" => $welcome
+));
+
+/**
+* or, use: $t->pparse("out", array("main"));
+*/
+$t->parse("out", array("main"));
+$t->p("out");
+?>
