c9e3d819077f858b2608e887da2b9527d7291265
Bjoern Schotte <bjoern@php.net>
2003-05-29 13:09:55 +0000

    use of blocks and nested templates
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_PHPLIB/trunk@129149 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/block.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once 'HTML/Template/PHPLIB.php';
+
+$articles = array(
+	"ISBN345624345" => "Matrix 2",
+	"ISBN45df32342" => "Roger Rabbit",
+	"ISBN305632232" => "foo bar",
+	"ISBN674589123" => "Joe user's adventures"
+);
+
+$t =& new Template_PHPLIB("./", "keep");
+
+$t->setFile(array(
+	"main" => "tpl1.ihtml",
+	"block" => "block.ihtml"
+));
+
+$t->setBlock("block","articlerow","ar");
+
+$t->setVar(array(
+	"TITLE" => "This is a block integrated into another template",
+	"BGCOLOR" => "#cccccc",
+	"BLOCKER" => "block"
+));
+
+foreach ($articles as $isbn => $name) {
+	$t->setVar(array(
+		"ID" => $isbn,
+		"ANAME" => $name
+	));
+	/**
+	* easy to use rule of thumb: first parameter is the last
+	* from setBlock, second is the second from setBlock, third
+	* is TRUE in order to append the parsed data to the template var
+	* articlerow
+	*/
+	$t->parse("ar", "articlerow", TRUE);
+}
+
+$t->parse("CONTENT", "block");
+$t->pparse("out",array("main"));
+?>
