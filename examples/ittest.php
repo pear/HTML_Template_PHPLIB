a88978e82020c5505ea1c5ebb8fb633def023ad2
Bjoern Schotte <bjoern@php.net>
2003-06-03 12:15:02 +0000

    try to benchmark against IT[X]
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_PHPLIB/trunk@129959 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/ittest.php
@@ -0,0 +1,33 @@
+<?php
+include_once 'HTML/Template/IT.php';
+include_once 'Benchmark/Timer.php';
+
+$articles = array(
+        "ISBN345624345" => "Matrix 2",
+        "ISBN45df32342" => "Roger Rabbit",
+        "ISBN305632232" => "foo bar",
+        "ISBN674589123" => "Joe user's adventures"
+);
+
+
+$t =& new HTML_Template_IT("./");
+$timer =& new Benchmark_Timer();
+$timer->start();
+
+$t->loadTemplateFile("block.ihtml",true,true);
+$t->setVariable(array(
+        "TITLE" => "This is a block integrated into another template",
+        "BGCOLOR" => "#cccccc",
+        "BLOCKER" => "block"
+));
+
+foreach ($articles as $k => $v) {
+	$t->setCurrentBlock("articlerow");
+	$t->setVariable("ID", $k);
+	$t->setVariable("ANAME", $v);
+	$t->parseCurrentBlock("articlerow");
+}
+$t->show();
+$timer->stop();
+$timer->display();
+?>
