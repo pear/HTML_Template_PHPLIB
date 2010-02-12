2011a95379d82d79282ab1559851dfa32dd2d6e1
Daniel O'Connor <clockwerx@php.net>
2010-02-12 02:32:17 +0000

    Fixed AllTests.php
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Template_PHPLIB/trunk@294955 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -3,11 +3,9 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'HTML_Template_PHPLIB_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 
-chdir(dirname(__FILE__) . '/../');
 require_once 'HTML_Template_PHPLIBTest.php';
 require_once 'HTML_Template_PHPLIB_GeneratorTest.php';
 require_once 'HTML_Template_PHPLIB_HelperTest.php';
